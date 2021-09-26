155c4e1b17ef5b39afe01e2a4c2197feec08f1e3
fanhuilin <fanhuilinl@163.com>
2021-09-26 23:21:26 +0800

    面向对象属性、方法

---
+++ day08/src/com/atguigu/java/OOPTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.java;
+
+/*
+* 一、Java面向对象学习三条主线
+* 1.Java类及类成员：属性、方法、构造器、代码块、内部类
+* 2.面向对象的三大特征：封装、继承、多态
+* 3.其他关键字：this、super、static、final、abstract、interface、package、import等
+*
+*
+*
+*
+*
+* */
+public class OOPTest {
+    public static void main(String[] args) {
+
+    }
+}
