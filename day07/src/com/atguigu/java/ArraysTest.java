c4f2e2a53ef3877ad1bc4181447d736b2a99e7b1
DESKTOP-S26BPHO\admin <fanhuilinl@163.com>
2021-09-26 18:12:00 +0800

    数组完结

---
+++ day07/src/com/atguigu/java/ArraysTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.java;
+
+import java.util.Arrays;
+
+/*
+* java.util.Arrays:操作数组的工具类，里面定义了很多操作数组的方法
+*
+*
+*
+*
+*
+* */
+public class ArraysTest {
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1,2,3,4};
+        System.out.println(Arrays.toString(arr1));
+    }
+}
